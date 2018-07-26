3a322dc81526f7e51f30d9a13224d967d74ff67b
sasmitoh <sasmitohrr@gmail.com>
2018-07-26 09:25:59 +0700

    v-01

---
+++ project/header.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <!-- Google Fonts -->
+    <!-- <link href="http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,400,700,600" rel="stylesheet" type="text/css">
+    <link href="http://fonts.googleapis.com/css?family=Open+Sans+Condensed:300,300italic,700" rel="stylesheet" type="text/css"> -->
+
+    <!-- berikut adalah pemagilan file .css -->
+    <meta name="description" content="belajr ngoding di clubs study ngoding" />
+    <link href="style.css" type="text/css" rel="stylesheet" />
+    <meta name="viewport" content="width=device-widget, initial-scale=1">
+    <title>kulgram</title>
+  </head>
+  <body>
+    <!-- ini adalah container -->
+    <div id="container">
+      <!-- membuat header -->
+      <header>
+        <div class="entry">
+          <h2>NGODING STUDY CLUB</h2>
+          <img src="image/group.jpg" />
+          <p>Group dimana tempat dimana para calon start-up semangat belajar.
+          yang akan mengharumkan nama kampus dan akan memmbuat masa depan menjadi indah, mau belajar ngoding? ya di sini tempatnya
+          belajar ngoding</p>
+
+        </div>
+      </header>
